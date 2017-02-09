b8b7285e349fb21f10b36364eef543a1cdafab4e
Icenowy Zheng <icenowy@aosc.xyz>
2017-02-09 13:53:11 +0800

    pwm: sun4i: add compatibles for A64's two PWM controllers
    Allwinner A64 SoC have two PWM controllers -- one in the normal block,
    and another in the "CPUs" block.
    
    Add compatibles for these two PWM controllers.
    
    Signed-off-by: Icenowy Zheng <icenowy@aosc.xyz>

--- drivers/pwm/pwm-sun4i.c
+++ drivers/pwm/pwm-sun4i.c
@@ -290,6 +290,18 @@ static const struct sun4i_pwm_data sun4i_pwm_data_h3 = {
 	.npwm = 1,
 };
 
+static const struct sun4i_pwm_data sun4i_pwm_data_a64 = {
+	.has_prescaler_bypass = true,
+	.has_rdy = true,
+	.npwm = 1,
+};
+
+static const struct sun4i_pwm_data sun4i_pwm_data_a64_r = {
+	.has_prescaler_bypass = true,
+	.has_rdy = true,
+	.npwm = 1,
+};
+
 static const struct of_device_id sun4i_pwm_dt_ids[] = {
 	{
 		.compatible = "allwinner,sun4i-a10-pwm",
@@ -306,6 +318,12 @@ static const struct of_device_id sun4i_pwm_dt_ids[] = {
 	}, {
 		.compatible = "allwinner,sun8i-h3-pwm",
 		.data = &sun4i_pwm_data_h3,
+	}, {
+		.compatible = "allwinner,sun50i-a64-pwm",
+		.data = &sun4i_pwm_data_a64,
+	}, {
+		.compatible = "allwinner,sun50i-a64-r-pwm",
+		.data = &sun4i_pwm_data_a64_r,
 	}, {
 		/* sentinel */
 	},
