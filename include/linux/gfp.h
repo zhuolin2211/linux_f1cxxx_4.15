c99b0dcec39a4862f9f50951e2d3a2fe096f6db9
Nigel Cunningham <nigel@nigelcunningham.com.au>
2017-09-30 11:52:28 +1000

    Correct TOI_NOTRACK bit colliding with NOLOCKDEP bit
    Signed-off-by: Nigel Cunningham <nigel@nigelcunningham.com.au>

--- include/linux/gfp.h
+++ include/linux/gfp.h
@@ -42,7 +42,7 @@ struct vm_area_struct;
 #define ___GFP_KSWAPD_RECLAIM	0x1000000u
 #define ___GFP_TOI_NOTRACK    0x2000000u
 #ifdef CONFIG_LOCKDEP
-#define ___GFP_NOLOCKDEP	0x2000000u
+#define ___GFP_NOLOCKDEP	0x4000000u
 #else
 #define ___GFP_NOLOCKDEP	0
 #endif
@@ -220,7 +220,7 @@ struct vm_area_struct;
 #define __GFP_NOLOCKDEP ((__force gfp_t)___GFP_NOLOCKDEP)
 
 /* Room for N __GFP_FOO bits */
-#define __GFP_BITS_SHIFT (25 + IS_ENABLED(CONFIG_LOCKDEP))
+#define __GFP_BITS_SHIFT (26 + IS_ENABLED(CONFIG_LOCKDEP))
 #define __GFP_BITS_MASK ((__force gfp_t)((1 << __GFP_BITS_SHIFT) - 1))
 
 /*
