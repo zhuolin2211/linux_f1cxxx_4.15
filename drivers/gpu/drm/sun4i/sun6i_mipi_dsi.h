7c60ad292771e48f6090c46eab5c5605ae384fa0
Jagan Teki <jagan@amarulasolutions.com>
2019-01-26 19:37:40 +0530

    drm/sun4i: sun6i_mipi_dsi: Add has_mod_clk quirk
    As per the user manual, look like mod clock is not mandatory
    for all Allwinner MIPI DSI controllers, it is connected to
    CLK_DSI_SCLK for A31 and not available in A64.
    
    So add has_mod_clk quirk and process the clk accordingly.
    
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/gpu/drm/sun4i/sun6i_mipi_dsi.h
+++ drivers/gpu/drm/sun4i/sun6i_mipi_dsi.h
@@ -17,6 +17,10 @@
 
 #include <linux/regulator/consumer.h>
 
+struct sun6i_dsi_variant {
+	bool			has_mod_clk;
+};
+
 struct sun6i_dsi {
 	struct drm_connector	connector;
 	struct drm_encoder	encoder;
@@ -33,6 +37,7 @@ struct sun6i_dsi {
 	struct sun4i_drv	*drv;
 	struct mipi_dsi_device	*device;
 	struct drm_panel	*panel;
+	const struct sun6i_dsi_variant	*variant;
 };
 
 static inline struct sun6i_dsi *host_to_sun6i_dsi(struct mipi_dsi_host *host)
